a3560aa1e378343c84e202ab290c13f01557515e
TusharPrajapat <tusharprajapat41@gmail.com>
2025-12-18 16:36:40 +0530

    Max. Length chain of Pairs

---
+++ Greedy_Algorithm/Max_Length_Chain_Of_Pairs.java
@@ -0,0 +1,27 @@
+package My_DSA_Journey.Greedy_Algorithm;
+
+import java.util.*;
+
+public class Max_Length_Chain_Of_Pairs {
+    public static int findLongestChain(int[][] pairs) {
+        Arrays.sort(pairs, Comparator.comparingDouble(o -> o[1]));
+
+        int chainLen = 1;
+        int chainEnd = pairs[0][1];
+
+        for(int  i = 1; i<pairs.length; i++){
+            if(pairs[i][0] > chainEnd){
+                chainLen++;
+                chainEnd = pairs[i][1];
+            }
+        }
+
+        return chainLen;
+    }
+
+    public static void main(String[] args){
+        int[][] pairs = {{5,24},{39,60},{5,28},{27,40},{50,90}};
+
+        System.out.println(findLongestChain(pairs));
+    }
+}
