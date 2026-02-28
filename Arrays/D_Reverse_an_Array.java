6e01b1eebc4646b42e50eb9edac03c5944ea3e2a
TusharPrajapat <tusharprajapat41@gmail.com>
2026-02-28 15:25:43 +0530

    Reverse an Array

---
+++ Arrays/D_Reverse_an_Array.java
@@ -0,0 +1,33 @@
+package My_DSA_Journey.Arrays;
+
+public class D_Reverse_an_Array {
+    public static void Reverse_Array(int arr[]) {
+        int first = 0;
+        int last = arr.length - 1;
+
+        while (first < last) {
+            int temp = arr[last];
+            arr[last] = arr[first];
+            arr[first] = temp;
+
+            first++;
+            last--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {2, 4, 6, 8, 10};
+        System.out.print("Before : ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        Reverse_Array(arr);
+
+        System.out.print("After : ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
