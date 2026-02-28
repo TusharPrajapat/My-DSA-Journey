e524e0b96292c342d60ced5f924bd51d037c7498
TusharPrajapat <tusharprajapat41@gmail.com>
2026-02-28 15:32:28 +0530

    Pairs in Array

---
+++ Arrays/E_Pairs_in_Array.java
@@ -0,0 +1,20 @@
+package My_DSA_Journey.Arrays;
+
+public class E_Pairs_in_Array {
+    public static void printPairs(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            int curr = numbers[i]; // 2, 4, 6, 8, 10
+            for (int j = i + 1; j < numbers.length; j++) {
+                System.out.print("(" + curr + "," + numbers[j] + ") ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {2, 4, 6, 8, 10};
+        printPairs(numbers);
+    }
+}
+
+
