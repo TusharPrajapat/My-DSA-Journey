f418c06b5d089786d32f516f44bd5537bf3b13ae
TusharPrajapat <tusharprajapat41@gmail.com>
2026-03-01 23:21:47 +0530

    Max SubArray Sum

---
+++ Arrays/Part2/A_MaxSub_Array_Sum/A_Brute_Force.java
@@ -0,0 +1,44 @@
+package My_DSA_Journey.Arrays.Part2.A_MaxSub_Array_Sum;
+
+public class A_Brute_Force {
+    public static void Print_SubArrays(int num[]) {
+        int largest = Integer.MIN_VALUE;
+        int smallest = Integer.MAX_VALUE;
+
+        for (int i = 0; i < num.length; i++) {
+            int sum = 0;
+//            int curr = num[i];
+//            System.out.println(curr);
+
+            for (int j = i; j < num.length; j++) {
+
+                for (int k = i; k <= j; k++) {
+                    System.out.print(num[k] + " ");
+                    sum = sum + num[k];
+                }
+
+                System.out.print("Sum : " + sum);
+
+                if (largest < sum) {
+                    largest = sum;
+                }
+
+                if (smallest > sum) {
+                    smallest = sum;
+                }
+
+                sum = 0;
+                System.out.println();
+            }
+            System.out.println();
+        }
+
+        System.out.println("Largest Value : " + largest);
+        System.out.println("Smallest Value : " + smallest);
+    }
+
+    public static void main(String[] args) {
+        int num[] = {1, -2, 6, -1, 3};
+        Print_SubArrays(num);
+    }
+}
