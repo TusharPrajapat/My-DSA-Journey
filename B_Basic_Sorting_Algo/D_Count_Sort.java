e0bdce8f5ce18ba50104cec81008a4e35a53ffd1
TusharPrajapat <tusharprajapat41@gmail.com>
2026-03-19 10:55:55 +0530

    Count Sort

---
+++ B_Basic_Sorting_Algo/D_Count_Sort.java
@@ -0,0 +1,33 @@
+package My_DSA_Journey.B_Basic_Sorting_Algo;
+
+public class D_Count_Sort {
+    public static void count_sort(int[] arr) {
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+
+        int[] count = new int[largest + 1];
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+
+        int j = 0;
+        for (int i = 0; i < arr.length; i++) {
+            while (count[i] > 0) {
+                arr[j] = i;
+                count[i]--;
+                j++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 4, 1, 3, 2, 4, 3, 7};
+        count_sort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
