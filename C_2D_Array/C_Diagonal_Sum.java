88fd110fb42cd3a77d11c463d7e48ffb01a54cb1
TusharPrajapat <tusharprajapat41@gmail.com>
2026-03-24 23:21:27 +0530

    Diagonal Sum

---
+++ C_2D_Array/C_Diagonal_Sum.java
@@ -0,0 +1,26 @@
+package My_DSA_Journey.C_2D_Array;
+
+public class C_Diagonal_Sum {
+    public static void printDiagonal(int[][] matrix) {
+
+        int sum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (i == j) {
+                    sum += matrix[i][j];
+                } else if (i + j == matrix.length - 1) {
+                    sum += matrix[i][j];
+                }
+            }
+        }
+        System.out.println(sum);
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}};
+        printDiagonal(matrix);
+    }
+}
